35b69e51866dd932392de6bee967b0df9d44a3e9
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-06-05 23:53:18 +0200

    delete method completed

--- api/playlists/controllers.js
+++ api/playlists/controllers.js
@@ -53,7 +53,17 @@ const controllers = {
       res.json(data);
     });
   },
-  delete: (req, res) => {},
+  delete: (req, res) => {
+    const id = Number(req.params.id);
+    const sql = `DELETE FROM playlists WHERE playlistId = ${id}`;
+    db.run(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.send("DELETED").end();
+    });
+  },
 };
 
 module.exports = controllers;
