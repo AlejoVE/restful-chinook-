e7ee8f868da9a3ee37c97ce2662dfca18202f1f0
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-06-05 14:36:52 +0200

    getOne method completed

--- api/albums/controllers.js
+++ api/albums/controllers.js
@@ -1,27 +1,37 @@
-const db = require('../db-connection');
+const db = require("../db-connection");
 
 const controllers = {
   getAll: (req, res) => {
-
     const sql = `SELECT * FROM albums`;
 
     db.all(sql, (err, rows) => {
       if (err) {
-        res.status(400).json({ "error": err.message });
+        res.status(400).json({ error: err.message });
         return;
       }
 
-      res.json(rows)
+      res.json(rows);
     });
   },
-  getOne: (req, res) => { },
+  getOne: (req, res) => {
+    const sql = `SELECT * FROM albums WHERE albumId = ${req.params.id} `;
+
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+
+      res.json(rows);
+    });
+  },
   create: (req, res) => {
     // read row data from body
   },
   update: (req, res) => {
     // read row data from body
   },
-  delete: (req, res) => { }
-}
+  delete: (req, res) => {},
+};
 
 module.exports = controllers;
