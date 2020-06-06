1b177eab1a15c489269633aa21b9245ae25ec49b
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-06-06 13:47:03 +0200

    create method completed

--- api/tracks/controllers.js
+++ api/tracks/controllers.js
@@ -26,12 +26,44 @@ const controllers = {
     });
   },
   create: (req, res) => {
-    // read row data from body
+    const data = {
+      name: req.body.name,
+      albumId: req.body.albumId,
+      mediaTypeId: req.body.mediaTypeId,
+      genreId: req.body.genreId,
+      composer: req.body.composer,
+      milliseconds: req.body.milliseconds,
+      bytes: req.body.bytes,
+      unitPrice: req.body.unitPrice,
+    };
+
+    const sql = `INSERT INTO tracks (name, albumId, mediaTypeId, genreId, composer, milliseconds, bytes, unitPrice) VALUES ("${data.name}", ${data.albumId}, ${data.mediaTypeId}, ${data.genreId}, "${data.composer}", ${data.milliseconds}, ${data.bytes}, ${data.unitPrice})`;
+    console.log(sql);
+    db.run(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: "success",
+        data: data,
+      });
+    });
   },
   update: (req, res) => {
     // read row data from body
   },
-  delete: (req, res) => {},
+  delete: (req, res) => {
+    const id = Number(req.params.id);
+    const sql = `DELETE FROM tracks WHERE trackId = ${id}`;
+    db.run(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.send("DELETED").end();
+    });
+  },
 };
 
 module.exports = controllers;
